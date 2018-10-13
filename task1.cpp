8deb30d4001116fe69acc16d6cf68e6d53ac7565
1702025 <msalman98@yahoo.com>
2018-10-13 11:17:58 +0500

    first time

---
+++ task1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+class TollBooth
+{
+public:
+
+private:
+unsigned total_car;
+double amount_of_money;
+double total_loss;
+};
+int main(int argc, char const *argv[])
+{
+  
+  return 0;
+}
