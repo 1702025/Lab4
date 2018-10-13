b68effe23831822cbb73c553614802580b15c5bd
1702025 <msalman98@yahoo.com>
2018-10-13 11:23:11 +0500

    second time

--- task1.cpp
+++ task1.cpp
@@ -3,7 +3,22 @@ using namespace std;
 class TollBooth
 {
 public:
-
+  TollBooth(int car,double money,double loss)
+    {
+      total_car=car;
+      amount_of_money=money;
+      total_loss=loss;
+    }
+     void payingcar()
+     {
+        amount_of_money=amount_of_money+150.50;
+        total_car++;
+     }
+     void nopaycar()
+     {
+       total_loss=total_loss+150.50;
+       total_car++;
+     }
 private:
 unsigned total_car;
 double amount_of_money;
@@ -11,6 +26,6 @@ double total_loss;
 };
 int main(int argc, char const *argv[])
 {
-  
+
   return 0;
 }
